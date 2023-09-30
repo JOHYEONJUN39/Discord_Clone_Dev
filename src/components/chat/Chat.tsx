60b17508c4f13cc69bdd260f1d298e667d903681
NEMURENAIYORU <lhslhs301@gmail.com>
2023-09-30 19:59:53 +0900

    ✨ チャットコンポーネント生成

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './Chat.scss'
+import { ChatHeader } from './ChatHeader'
 
 export default function Chat() {
   return (
     <div className='chat'>
       {/* chat header */}
-
+      <ChatHeader />
       {/* chat messages */}
+      <div className='chatMessage'>
 
+      </div>
       {/* chat input */}
+      <div className='chatInput'>
+        
+      </div>
     </div>
   )
 }
