03178bcd98c1d6852df19cdadb69c149ae90365f
NEMURENAIYORU <lhslhs301@gmail.com>
2023-10-01 05:56:26 +0900

    ✨ Messagesのタイプ指定

---
+++ src/types/Messages.interface.ts
@@ -0,0 +1,12 @@
+import { Timestamp } from "firebase/firestore";
+
+export interface Messages {
+  timestamp: Timestamp;
+  message: string;
+  user: {
+    uid: string;
+    photo: string;
+    email: string;
+    displayName: string;
+  };
+}
