5c61f2729baaa0418157b56b39dd6dad6bbd2f50
NEMURENAIYORU <lhslhs301@gmail.com>
2023-09-30 17:51:21 +0900

    ✨ サイドバーのスタイリング及びアイコン追加

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Sidebar.scss'
+
+import {  ExpandMore, Add } from '@mui/icons-material/';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      {/* sidebar Left */}
+      <div className='sidebarLeft'>
+        <div className='serverIcon'>
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className='serverIcon'>
+          <img src="./logo192.png" alt="" />
+        </div>
+      </div>
+      {/* sidebar Right */}
+      <div className='sidebarRight'>
+        <div className='sidebarTop'>
+          <h3>Discord</h3>
+          <ExpandMore />
+        </div>
+
+        {/* sidebar Channels */}
+        <div className='sidebarChannels'>
+          <div className='sidebarChannelsHeader'>
+            <div className='sidebarHeader'>
+              <ExpandMore />
+              <h4>프로그래밍 채널</h4>
+            </div>
+            <Add className='sidebarAddIcon' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
