3b760877ed8655f867ea4a23d93f5764868c8ed6
NEMURENAIYORU <lhslhs301@gmail.com>
2023-10-01 01:42:48 +0900

    ✨ dispatchの状態にそってlogin&logout

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import './App.scss';
 import Chat from './components/chat/Chat';
 import Sidebar from './components/sidebar/Sidebar';
 import Login from './components/login/Login';
-import { useAppSelector } from './app/hooks';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { login, logout } from './features/userSlice';
 
 function App() {
 
   const user = useAppSelector((state : any) => state.user);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log(authUser);
+      if(authUser) {
+        dispatch(login({
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          displayName: authUser.displayName,
+        }));
+      }
+      else {
+        dispatch(logout());
+      }
+    })
+  }, [dispatch])
   
 
   return (
