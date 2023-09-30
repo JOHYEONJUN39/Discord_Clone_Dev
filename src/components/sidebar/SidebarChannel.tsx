f1f946cc9981adc6729e08e2993f82537e749739
NEMURENAIYORU <lhslhs301@gmail.com>
2023-10-01 03:53:38 +0900

    ✨ 受け取ったpropsでチャンネルネームを表示

--- src/components/sidebar/SidebarChannel.tsx
+++ src/components/sidebar/SidebarChannel.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './SidebarChannel.scss'
+import { Channel } from '../../types/Channel.interface';
 
-const SidebarChannel = () => {
+const SidebarChannel = (props: Channel) => {
+  const { id, channel } = props;
+  console.log(channel);
+
   return (
     <div className='sidebarChannel'>
       <h4>
         <span className='sidebarChannelHash'>#</span>
-        Udemy
+        {channel.channel.channelName}
       </h4>
     </div>
   )
