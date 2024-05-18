f2b72247a7721112588a22e6904e6284f91eb2e3
NEMURENAIYORU <lhslhs301@gmail.com>
2024-05-18 19:00:49 +0900

    🔨 chore : ウェブでのCollection削除は危険なので除外

--- src/components/sidebar/SidebarChannel.tsx
+++ src/components/sidebar/SidebarChannel.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
 import './SidebarChannel.scss'
 import { Channel } from '../../types/Channel.interface';
 import { useAppDispatch } from '../../app/hooks';
 import { setChannelInfo } from '../../features/channelSlice';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 const SidebarChannel = (props: Channel) => {
   const { id, channel } = props;
   const dispatch = useAppDispatch();
+
+  const deleteChannel = async () => {
+    // web에서 collection을 삭제하는 것은 보안 문제가 있기 때문에 보류
+    // let channelName: boolean = window.confirm("新しいチャンネルを作成します。");
+  
+    // if(channelName) {
+    //   // collection의 "channels"에서 해당 채널 삭제
+    //   await deleteDoc(doc(db, "channels", id));
+    // }
+    alert("権限がございません。")
+  }
 
   return (
     <div className='sidebarChannel' onClick={() => dispatch(setChannelInfo({
@@ -15,7 +26,8 @@ const SidebarChannel = (props: Channel) => {
     }))}>
       <h4>
         <span className='sidebarChannelHash'>#</span>
-        {channel.channel.channelName}
+        <span className="channelName">{channel.channel.channelName}</span>
+        <DeleteForeverIcon onClick={() => deleteChannel()} />
       </h4>
     </div>
   )
